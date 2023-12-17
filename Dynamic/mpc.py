fe73314b904ffcbe05340d7b98e831cba98248db
RemiMak <remimakinwa@outlook.com>
2023-12-17 18:22:55 +0100

    Cleaned up code

--- Dynamic/mpc.py
+++ Dynamic/mpc.py
@@ -19,13 +19,6 @@ def mpc_control(quadrotor, N, x_init, x_target, num_states=4, num_inputs=2):
     # Create an optimization problem
     opti = ca.Opti()
 
-    # State & Input matrix
-    weight_pos = np.eye(3) * 120
-    weight_att = np.eye(3) * 20
-    weight_vel = np.eye(3) * 0
-    weight_omega = np.eye(3) * 0
-    R = np.eye(num_inputs) * 0.1
-
     # Define Variables
     x = opti.variable(num_states, N + 1)
     u = opti.variable(num_inputs, N) 
@@ -33,8 +26,30 @@ def mpc_control(quadrotor, N, x_init, x_target, num_states=4, num_inputs=2):
     # Initialize Cost & Constraints
     cost = 0.0
     constraints = []
-    time = 0
 
+    # Motor Limit
+    rpm_lim = 1000
+    
+    # Weights
+    weights = {
+        "positions": 120,
+        "attitudes": 20,
+        "velocities": 0,
+        "ang_velocities": 0,
+        "inputs": 0.1
+    }
+
+    Q = ca.diagcat(
+        np.eye(3) * weights["positions"],
+        np.eye(3) * weights["attitudes"],
+        np.eye(3) * weights["velocities"],
+        np.eye(3) * weights["ang_velocities"]
+    )
+
+    R = np.eye(num_inputs) * weights["inputs"]
+
+    
+    time = 0
     #Loop through time steps
     for k in range(N):
         time += quadrotor.dt
@@ -43,17 +58,14 @@ def mpc_control(quadrotor, N, x_init, x_target, num_states=4, num_inputs=2):
         constraints += [x[:, k+1] == quadrotor.calculate_next_step(x[:, k], u[:, k])]
 
         # Motor Limits
-        rpm_lim = 1000
-        opti.subject_to(opti.bounded(0, u[0, k], rpm_lim))
-        opti.subject_to(opti.bounded(0, u[1, k], rpm_lim))
-        opti.subject_to(opti.bounded(0, u[2, k], rpm_lim))
-        opti.subject_to(opti.bounded(0, u[3, k], rpm_lim))
+        for i in range(4):
+            opti.subject_to(opti.bounded(0, u[i, k], rpm_lim))
 
         # Cost function
         x_target[0:2] = ref(time, 3)
         e_k = x_target - x[:, k]
         e_k[3:6] = angle_difference(x_target[3:6], x[3:6, k])
-        Q = ca.diagcat(weight_pos, weight_att, weight_vel, weight_omega)
+        
         cost += ca.mtimes(e_k.T, ca.mtimes(Q, e_k)) + ca.mtimes(u[:, k].T, ca.mtimes(R, u[:, k]))
 
     
@@ -78,139 +90,68 @@ def mpc_control(quadrotor, N, x_init, x_target, num_states=4, num_inputs=2):
 
     return optimal_solution_x, optimal_solution_u
 
+dt = 0.01
 x_init = np.zeros(12)
 x_target = np.zeros(12)
 x_target[0:3] = [0, 0, 0]
-dt = 0.01
 
-results = mpc_control(Quadrotor(dt), 300, x_init, x_target, 12, 4)
+results = mpc_control(Quadrotor(dt), 200, x_init, x_target, 12, 4)
 positions = results[0][0:6, :]
 inputs = results[1]
 
-# Extract x, y, z coordinates
-x_positions = positions[0, :]
-y_positions = positions[1, :]
-z_positions = positions[2, :]
-x_rot = positions[3, :]
-y_rot = positions[4, :]
-z_rot = positions[5, :]
+# Extract x, y, z coordinates and rotations
+x_positions, y_positions, z_positions, x_rot, y_rot, z_rot = positions
 
 # Control inputs
-rotor_1 = inputs[0, :]
-rotor_2 = inputs[1, :]
-rotor_3 = inputs[2, :]
-rotor_4 = inputs[3, :]
+rotor_1, rotor_2, rotor_3, rotor_4 = inputs
 
 def animate_trajectory():
-    # Function to convert Euler angles to a rotation matrix
     def rotation_matrix_from_euler(x, y, z):
-        # Convert angles from degrees to radians
-
-        # Compute rotation matrix
         Rx = np.array([[1, 0, 0], [0, np.cos(x), -np.sin(x)], [0, np.sin(x), np.cos(x)]])
         Ry = np.array([[np.cos(y), 0, np.sin(y)], [0, 1, 0], [-np.sin(y), 0, np.cos(y)]])
         Rz = np.array([[np.cos(z), -np.sin(z), 0], [np.sin(z), np.cos(z), 0], [0, 0, 1]])
-
-        R = np.dot(Rz, np.dot(Ry, Rx))
-
-        return R
-
-    # Function to draw the quadrotor
-    def draw_quadrotor(ax, position, rotation):
-        # Define the quadrotor's size
-        size = 0.5
-        size_x = size_y = size  # for x and z dimensions
-        size_z = size / 2  # for y dimension, make it thinner
-
-        # Define the vertices of the quadrotor
-        vertices = np.array([[-size_x, -size_y, -size_z],
-                            [size_x, -size_y, -size_z],
-                            [size_x, size_y, -size_z],
-                            [-size_x, size_y, -size_z],
-                            [-size_x, -size_y, size_z],
-                            [size_x, -size_y, size_z],
-                            [size_x, size_y, size_z],
-                            [-size_x, size_y, size_z]])
-
-        # Rotate and translate the vertices based on the quadrotor's position and orientation
-        vertices = np.dot(vertices, rotation.T) + position
-
-        # Define the faces of the quadrotor
-        faces = [[vertices[j] for j in face] for face in [[0, 1, 5, 4], [7, 6, 2, 3], [0, 1, 2, 3], [7, 6, 5, 4], [0, 3, 7, 4], [1, 2, 6, 5]]]
-
-        # Define the colors for each face
-        facecolors = ['cyan', 'cyan', 'red', 'cyan', 'cyan', 'cyan']
-
-        # Draw the quadrotor
-        ax.add_collection3d(Poly3DCollection(faces, facecolors=facecolors, linewidths=1, edgecolors='r', alpha=1))
-
+        return np.dot(Rz, np.dot(Ry, Rx))
 
     def set_axes_equal(ax):
-        '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
-        cubes as cubes, etc..  This is one possible solution to Matplotlib's
-        ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
-        '''
+        limits = np.array([ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()])
+        origin = np.mean(limits, axis=1)
+        radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+        ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
+        ax.set_ylim3d([origin[1] - radius, origin[1] + radius])
+        ax.set_zlim3d([origin[2] - radius, origin[2] + radius])
 
-        x_limits = ax.get_xlim3d()
-        y_limits = ax.get_ylim3d()
-        z_limits = ax.get_zlim3d()
-
-        x_range = abs(x_limits[1] - x_limits[0])
-        x_middle = np.mean(x_limits)
-        y_range = abs(y_limits[1] - y_limits[0])
-        y_middle = np.mean(y_limits)
-        z_range = abs(z_limits[1] - z_limits[0])
-        z_middle = np.mean(z_limits)
-
-        # The plot bounding box is a sphere in the sense of the infinity
-        # norm, hence I call half the max range the plot radius.
-        plot_radius = 0.5*max([x_range, y_range, z_range])
-
-        ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-        ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-        ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
+    size = 0.5
+    vertices = size * np.array([[-1, -1, -0.5], [1, -1, -0.5], [1, 1, -0.5], [-1, 1, -0.5], [-1, -1, 0.5], [1, -1, 0.5], [1, 1, 0.5], [-1, 1, 0.5]])
+    faces = [[0, 1, 5, 4], [7, 6, 2, 3], [0, 1, 2, 3], [7, 6, 5, 4], [0, 3, 7, 4], [1, 2, 6, 5]]
+    facecolors = ['cyan', 'cyan', 'red', 'cyan', 'cyan', 'cyan']
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
     def animate(i):
         ax.clear()
-        draw_quadrotor(ax, [x_positions[i], y_positions[i], z_positions[i]], rotation_matrix_from_euler(x_rot[i], y_rot[i], z_rot[i]))
+        position = np.array([x_positions[i], y_positions[i], z_positions[i]])
+        rotation = rotation_matrix_from_euler(x_rot[i], y_rot[i], z_rot[i])
+        rotated_vertices = np.dot(vertices, rotation.T) + position
+        rotated_faces = [[rotated_vertices[j] for j in face] for face in faces]
+        ax.add_collection3d(Poly3DCollection(rotated_faces, facecolors=facecolors, linewidths=1, edgecolors='r', alpha=1))
         ax.set_xlim([min(x_positions), max(x_positions)])
         ax.set_ylim([min(y_positions), max(y_positions)])
         ax.set_zlim([min(z_positions), max(z_positions)])
         set_axes_equal(ax)
 
-    print(len(x_positions))
     ani = animation.FuncAnimation(fig, animate, frames=len(x_positions), interval=dt * 1000)
-    # ani.save('animation.mp4', writer='ffmpeg', fps=60)
-
     plt.show()
 
 def plot_control_inputs():
     # Plot control inputs
     plt.figure()
 
-    # Plot control input for rotor 1
-    plt.subplot(4, 1, 1)
-    plt.plot(rotor_1)
-    plt.title('Control Input for Rotor 1')
-
-    # Plot control input for rotor 2
-    plt.subplot(4, 1, 2)
-    plt.plot(rotor_2)
-    plt.title('Control Input for Rotor 2')
-
-    # Plot control input for rotor 3
-    plt.subplot(4, 1, 3)
-    plt.plot(rotor_3)
-    plt.title('Control Input for Rotor 3')
-
-    # Plot control input for rotor 4
-    plt.subplot(4, 1, 4)
-    plt.plot(rotor_4)
-    plt.title('Control Input for Rotor 4')
+    # Plot control input for each rotor
+    for i, rotor in enumerate([rotor_1, rotor_2, rotor_3, rotor_4], start=1):
+        plt.subplot(4, 1, i)
+        plt.plot(rotor)
+        plt.title(f'Control Input for Rotor {i}')
 
     plt.tight_layout()
     plt.show()
