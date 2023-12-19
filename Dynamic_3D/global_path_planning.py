91ff59a53cbdf4e27b8741726acaae186529ed61
Anton Lang <34992832+alang321@users.noreply.github.com>
2023-12-19 09:47:53 +0100

    add untested rrt start 3d implementation

--- Dynamic_3D/global_path_planning.py
+++ Dynamic_3D/global_path_planning.py
@@ -1,19 +1,14 @@
-
 from ipywidgets import IntProgress
 from IPython.display import display
 import numpy as np
 import matplotlib.pyplot as plt
 import operator
 
-def rrt_star(occupany_grid, start, goal, radius, max_iter=1000, report_progress=True):
+def rrt_star(world_3d, start, goal, radius, max_iter=1000, report_progress=True):
     # Initialize start and goal node
-    start_idx = occupany_grid.get_closest_grid_index(start)
-    goal_idx = occupany_grid.get_closest_grid_index(goal)
-    start_node = Node(None, start_idx)
-    goal_node = Node(None, goal_idx)
+    start_node = Node(None, start)
+    goal_node = Node(None, goal)
     start_node.g = 0
-
-    visited_points = np.zeros(occupany_grid.grid_map.shape)
 
     nodes = [start_node]
 
@@ -31,32 +26,26 @@ def rrt_star(occupany_grid, start, goal, radius, max_iter=1000, report_progress=
             if i % 10 == 0:
                 f.value = i
 
-        rnd_idx = occupany_grid.get_random_cell_idx()
+        rnd_pos = world_3d.get_random_point()
 
-        #is the random point already visitedä
-        if visited_points[rnd_idx] == 1:
-            continue
-
-        visited_points[rnd_idx] = 1
-
-        if occupany_grid.is_occupied_index(*rnd_idx):
+        if world_3d.is_colliding(rnd_pos):
             continue
 
         # Find nearest valid node
         nearest_node = None
         nearest_dist_sq = None
-        dist_sq = [(node.index[0] - rnd_idx[0]) ** 2 + (node.index[1] - rnd_idx[1]) ** 2 for node in nodes]
+        dist_sq = [(node.position[0] - rnd_pos[0]) ** 2 + (node.position[1] - rnd_pos[1]) ** 2 for node in nodes]
 
         sorted_nodes = [(x, dist) for dist, x in sorted(zip(dist_sq, nodes), key=lambda pair: pair[0])]
         for node, dist in sorted_nodes:
-            if not occupany_grid.is_line_intersecting_obstacle(*node.index, *rnd_idx):
+            if not world_3d.is_line_colliding(*node.psoition, *rnd_pos):
                 nearest_node = node
                 nearest_dist_sq = dist
 
         if nearest_node is None:
             continue
 
-        new_node = Node(nearest_node, rnd_idx)
+        new_node = Node(nearest_node, rnd_pos)
         new_node.g = nearest_dist_sq ** 0.5 + nearest_node.g
 
         # check if nearby nodes are close to the new node
@@ -65,10 +54,11 @@ def rrt_star(occupany_grid, start, goal, radius, max_iter=1000, report_progress=
             if dist_sq > radius_sq:
                 continue
 
-            # check if new node is closer to the node
-            if occupany_grid.is_line_intersecting_obstacle(*node.index, *new_node.index):
+            # check if line is colliding
+            if world_3d.is_line_colliding(*node.position, *new_node.position):
                 continue
 
+            # check if new node is closer path to the node
             dist = dist_sq ** 0.5
             if new_node.g + dist < node.g:
                 nodes[idx].parent = new_node
@@ -78,7 +68,7 @@ def rrt_star(occupany_grid, start, goal, radius, max_iter=1000, report_progress=
 
         dist_sq = new_node.distance_sq(goal_node)
         if dist_sq < 2 * radius_sq:
-            if not occupany_grid.is_line_intersecting_obstacle(*new_node.index, *goal_node.index):
+            if not world_3d.is_line_colliding(*new_node.position, *goal_node.position):
                 dist = dist_sq ** 0.5
                 if goal_node.g is None or new_node.g + dist < goal_node.g:
                     goal_node.parent = new_node
@@ -93,255 +83,84 @@ def rrt_star(occupany_grid, start, goal, radius, max_iter=1000, report_progress=
         current = goal_node
         i = 0
         while current is not None:
-            path.append(current.index)
+            path.append(current.position)
             current = current.parent
             i += 1
             if i > max_iter:
                 print("Infinite loop")
-                return Path(None, start, goal, "RRT*", occupany_grid, False)
-        return Path(path[::-1], start, goal, "RRT*", occupany_grid, success)
+                return Path(None, start, goal, "RRT*", world_3d, False)
+        return Path(path[::-1], start, goal, "RRT*", world_3d, success)
     else:
         print("No path found")
-        return Path(None, start, goal, "RRT*", occupany_grid, success)
-
-
-def a_star(occupany_grid, start, goal, report_progress=True):
-    def heuristic(node1, node2):
-        return (node1.index[0] - node2.index[0]) ** 2 + (node1.index[1] - node2.index[1]) ** 2
-
-    #lists for open and closed nodes
-    open_nodes = []
-    closed_nodes = []
-
-    # Initialize start and goal node
-    start_idx = occupany_grid.get_closest_grid_index(start)
-    goal_idx = occupany_grid.get_closest_grid_index(goal)
-    start_node = Node(None, start_idx)
-    goal_node = Node(None, goal_idx)
-    start_node.g = 0
-    start_node.h = 0
-    start_node.f = 0
-    goal_node.g = 0
-    goal_node.h = 0
-    goal_node.f = 0
-
-    open_nodes.append(start_node)
-
-    nodes_visited = 0
-    if report_progress:
-        total_nodes = occupany_grid.num_free_cells
-        f = IntProgress(min=0, max=total_nodes)  # instantiate the bar
-        display(f)  # display the bar
-    # Loop until goal is found
-    while len(open_nodes) > 0:
-        if(report_progress):
-            f.value = nodes_visited
-
-        # Get the cheapest open node measured by f value, f value is defined in the node class operator overloading
-        min_idx, cheapest_open_node = min(enumerate(open_nodes), key=operator.itemgetter(1))
-
-        # Move from open to closed list
-        closed_nodes.append(open_nodes.pop(min_idx))
-
-        # Check if goal is found
-        if cheapest_open_node == goal_node:
-            print("Goal found")
-            #reconstruct the path
-            path = []
-            current = cheapest_open_node
-            while current is not None:
-                path.append(current.index)
-                current = current.parent
-            return Path(path[::-1], start, goal, "A*", occupany_grid, True)
-
-        # Generate children
-        for neighbor_idx, cost in occupany_grid.get_adjacent_free_cells(*cheapest_open_node.index):
-            # Create neighbor node
-            neighbor_node = Node(cheapest_open_node, neighbor_idx)
-
-            # Check if neighbor is in closed list
-            if not neighbor_node in closed_nodes:
-                # Calculate f, g, and h values
-                neighbor_node.g = cheapest_open_node.g + cost
-                neighbor_node.h = heuristic(neighbor_node, cheapest_open_node)
-                neighbor_node.f = neighbor_node.g + neighbor_node.h
-
-                #check if neighbor is in open list
-                #if not a value error is thrown
-                try:
-                    index = open_nodes.index(neighbor_node)
-                    if open_nodes[index].g > neighbor_node.g:
-                        # if its cheaper to get to the current neighbor with the current path
-                        # update the open node, otherwise discard it
-                        open_nodes[index] = neighbor_node
-
-                except ValueError:
-                    # if the neighbor is not in the open list add it
-                    open_nodes.append(neighbor_node)
-                    nodes_visited += 1
-                    pass
-
-    return Path(None, start, goal, "A*", occupany_grid, False)
+        return Path(None, start, goal, "RRT*", world_3d, success)
 
 
 
 
-class OccupancyGrid:
-    def __init__(self, grid_resolution, origin, obstacles, field_size, obstacle_margin=0):
-        self.grid_resolution = grid_resolution
-        self.origin = origin
+class World_3D:
+    def __init__(self, x_range, y_range, z_range, obstacles, obstacle_margin=0):
+        self.x_range = x_range
+        self.y_range = y_range
+        self.z_range = z_range
         self.obstacles = obstacles
-        self.field_size = field_size
         self.obstacle_margin = obstacle_margin
 
-        self.x_points = np.arange(self.origin[0], self.field_size[0], self.grid_resolution)
-        self.y_points = np.arange(self.origin[1], self.field_size[1], self.grid_resolution)
-
-
-        self.diag_cost = (2 ** 0.5) * self.grid_resolution
-        self.straight_cost = self.grid_resolution
-
-        self.num_free_cells = 0
-        self.grid_map = self.generate_grid_map()
-
-
-    def get_closest_grid_index(self, point):
-        x_points = np.arange(self.origin[0], self.field_size[0], self.grid_resolution)
-        y_points = np.arange(self.origin[1], self.field_size[1], self.grid_resolution)
-        x_index = np.argmin(np.abs(x_points - point[0]))
-        y_index = np.argmin(np.abs(y_points - point[1]))
-        return x_index, y_index
-
-    def index_to_point(self, index_x, index_y):
-        x = self.x_points[index_x]
-        y = self.y_points[index_y]
-        return [x, y]
-
-    def generate_grid_map(self):
-        x_dim = self.x_points.size
-        y_dim = self.y_points.size
-        grid_map = np.zeros((x_dim, y_dim))
-        for i, x in enumerate(self.x_points):
-            for j, y in enumerate(self.y_points):
-                if self.is_colliding([x,y], self.grid_resolution / 2):
-                    grid_map[i, j] = 1
-                else:
-                    self.num_free_cells += 1
-        return grid_map
-
-    def is_colliding(self, point, radius):
+    def is_colliding(self, point, margin=0):
         for obstacle in self.obstacles:
             squared_dist = (point[0] - obstacle[0]) ** 2 + (point[1] - obstacle[1]) ** 2
-            if squared_dist <= (obstacle[2] + radius + self.obstacle_margin) ** 2:
+            if squared_dist <= (obstacle[2] + margin + self.obstacle_margin) ** 2:
                 return True
         return False
 
-    def is_line_intersecting_obstacle(self, x0, y0, x1, y1, plot=False):
-        # Bresenham's line algorithm
-        #https://jccraig.medium.com/we-must-draw-the-line-1820d49d19dd
-        x, y = x0, y0
-        dx = abs(x1 - x0)
-        dy = abs(y1 - y0)
-        dir_x = 1 if x0 < x1 else -1 if x0 > x1 else 0
-        dir_y = 1 if y0 < y1 else -1 if y0 > y1 else 0
-        incr_x = dy // 2
-        incr_y = dx // 2
-        iterations = dx + 1 if dx > dy else dy + 1
+    def is_line_colliding(self, x0, y0, x1, y1, margin=0, point_spacing=0.1):
+        #create points along the line with given spacing
+        dist = ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
+        point_fractions = np.arange(0, 1, point_spacing)
 
-        if plot:
-            plt.imshow(self.grid_map.T, cmap='Greys', origin='lower')
-            plt.xlabel('X-axis')
-            plt.ylabel('Y-axis')
-            intersection = False
+        for fraction in point_fractions:
+            x = x0 + fraction * (x1 - x0)
+            y = y0 + fraction * (y1 - y0)
+            if self.is_colliding((x, y), margin):
+                return True
 
-        for i in range(iterations):
-            if plot:
-                plt.scatter(x, y, color = "r")
-                if self.is_occupied_index(x, y):
-                    intersection = True
-            else:
-                if self.is_occupied_index(x, y):
-                    return True
-            incr_x += dx
-            if incr_x >= dy:
-                incr_x -= dy
-                x += dir_x
-            incr_y += dy
-            if incr_y >= dx:
-                incr_y -= dx
-                y += dir_y
-
-        if plot:
-            plt.title(('Intersection ' + str(intersection)))
-
-            #start and end points
-            plt.scatter(x0, y0, color = "g")
-            plt.scatter(x1, y1, color = "pink")
-
-            plt.show()
-
-            return intersection
+        if self.is_colliding((x1, y1), margin):
+            return True
 
         return False
 
-    def get_random_cell_idx(self):
-        index_x = np.random.randint(0, self.grid_map.shape[0])
-        index_y = np.random.randint(0, self.grid_map.shape[1])
-        return index_x, index_y
+    def get_random_point(self):
+        x = np.random.uniform(self.x_range[0], self.x_range[1])
+        y = np.random.uniform(self.y_range[0], self.y_range[1])
+        z = np.random.uniform(self.z_range[0], self.z_range[1])
+        return x, y, z
 
-    def get_adjacent_free_cells(self, index_x, index_y):
-        adjacent_indices = []
-        costs = []
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == j == 0:
-                    continue
-                new_index_x = index_x + i
-                new_index_y = index_y + j
-                if self.is_free_valid_cell(new_index_x, new_index_y):
-                    #check if diagonal or straight
-                    if i == 0 or j == 0:
-                        cost = self.straight_cost
-                    else:
-                        cost = self.diag_cost
-                    adjacent_indices.append(((new_index_x, new_index_y), cost))
-        return adjacent_indices
-
-    def is_free_valid_cell(self, index_x, index_y):
-        return index_x >= 0 and index_x < len(self.grid_map[0]) and index_y >= 0 and index_y < len(self.grid_map[1]) and self.grid_map[index_x, index_y] != 1
-
-    def is_occupied_index(self, index_x, index_y):
-        return self.grid_map[index_x, index_y] == 1
+    def is_in_constraints(self, pos_x, pos_y):
+        return pos_x >= self.x_range[0] and pos_x <= self.x_range[1] and pos_y >= self.y_range[0] and pos_y <= self.y_range[1]
 
     def plot(self):
-        #flip the axes to match the grid
-        plt.imshow(self.grid_map.T, cmap='Greys', origin='lower')
-        plt.xlabel('X-axis')
-        plt.ylabel('Y-axis')
-        plt.title('Occupancy Grid')
-        plt.show()
+        print("not implemented")
 
 
 class Node:
-    def __init__(self, parent, index):
+    def __init__(self, parent, position):
         self.parent = parent
-        self.index = index
+        self.position = position
 
         self.g = None # Distance to start node
         self.h = None # Heuristic
         self.f = None # Total cost, distance to start + heuristic
 
     def distance(self, other):
-        return (self.index[0] - other.index[0]) ** 2 + (self.index[1] - other.index[1]) ** 2
+        return (self.position[0] - other.position[0]) ** 2 + (self.position[1] - other.position[1]) ** 2
 
     def distance_sq(self, other):
-        return (self.index[0] - other.index[0]) ** 2 + (self.index[1] - other.index[1]) ** 2
+        return (self.position[0] - other.position[0]) ** 2 + (self.position[1] - other.position[1]) ** 2
 
     def __eq__(self, other):
-        return self.index == other.index
+        return self.position == other.position
 
     def __ne__(self, other):
-        return self.index != other.index
+        return self.position != other.position
 
     def __lt__(self, other):
         return self.f < other.f
@@ -359,55 +178,15 @@ class Node:
         return f'Node: {self.index}, g: {self.g}'
 
 class Path:
-    def __init__(self, path_indeces, start, point, name, occupany_grid, valid=True):
-        self.path_indeces = path_indeces
+    def __init__(self, path_points, start, goal, name, world_3d, valid=True):
         self.name = name
         self.start = start
-        self.point = point
+        self.goal = goal
         self.valid = valid
-        self.occupany_grid = occupany_grid
+        self.world_3d = world_3d
 
-        self.path_points = self.calculate_path_points()
-
-        if self.valid:
-            # convert indeces to points
-            path_points = []
-            for index in path_indeces:
-                path_points.append(occupany_grid.index_to_point(*index))
-            self.path_points = np.array(path_points)
-
-    def calculate_path_points(self):
-        if self.valid:
-            # convert indeces to points
-            path_points = []
-            for index in self.path_indeces:
-                path_points.append(self.occupany_grid.index_to_point(*index))
-            path_points = np.array(path_points)
-        return path_points
+        self.path_points = path_points
 
     def plot(self):
-        if self.valid:
-            pos_x = self.path_points[:, 0]
-            pos_y = self.path_points[:, 1]
-            plt.plot(pos_x, pos_y, color = "r", label = "Vehicle Path")
-
-        plt.title(self.name)
-        plt.xlabel('X-axis')
-        plt.ylabel('Y-axis')
-
-        #mark start and goal
-        plt.scatter(self.start[0], self.start[1], color = "g", label = "Start")
-        plt.scatter(self.point[0], self.point[1], color = "pink", label = "Goal")
-        plt.legend()
-
-        #plot obstacles
-        for idx, obstacle in enumerate(self.occupany_grid.obstacles):
-            circle = plt.Circle((obstacle[0], obstacle[1]), obstacle[2], color='blue', fill=False)
-            plt.gca().add_patch(circle)
-
-        # Set axis limits for x and y
-        plt.xlim(0, 50)
-        plt.ylim(0, 50)
-        plt.axis('equal')
-        plt.grid()
-        plt.show()
+        print("not implemented")
+        return
